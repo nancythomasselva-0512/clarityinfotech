c821d84ddf926159ac330fa14364b3fec84ea33a
Nancy Thomas <nancythomasselva0512@gmail.com>
2026-09-17 14:49:20 +0530

    Prepare site for Vercel deployment

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata = {
@@ -8,7 +9,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="font-sans h-full antialiased">
-      <body className="min-h-full flex flex-col font-sans">{children}</body>
+      <body className="min-h-full flex flex-col font-sans">
+        {children}
+        <div id="root"></div>
+        <Script
+          src="http://localhost:3000/loader.js"
+          data-api-key="PUB_67B32A79924D2EA331A4"
+          data-domain="localhost:3001"
+          data-api-url="http://localhost:3000"
+          strategy="afterInteractive"
+        />
+      </body>
     </html>
   );
 }
